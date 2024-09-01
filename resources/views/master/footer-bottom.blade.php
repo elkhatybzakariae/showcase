f987b7c986a599897d9a55e9ad13e04bfbe9b804
elkhatybzakariae <elkhatyb.zakariae@gmail.com>
2024-09-01 15:50:36 +0100

    A

---
+++ resources/views/master/footer-bottom.blade.php
@@ -0,0 +1,31 @@
+<div id="footer-bottom">
+      <div class="container">
+        <div class="row d-flex flex-wrap justify-content-between">
+          <div class="col-md-4 col-sm-6">
+            <div class="Shipping d-flex">
+              <p>We ship with:</p>
+              <div class="card-wrap ps-2">
+                <img src="{{ asset('storage/images/dhl.png') }}" alt="visa">
+                <img src="{{ asset('storage/images/shippingcard.png') }}" alt="mastercard">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="payment-method d-flex">
+              <p>Payment options:</p>
+              <div class="card-wrap ps-2">
+                <img src="{{ asset('storage/images/visa.jpg') }}" alt="visa">
+                <img src="{{ asset('storage/images/mastercard.jpg') }}" alt="mastercard">
+                <img src="{{ asset('storage/images/paypal.jpg') }}" alt="paypal">
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6">
+            <div class="copyright">
+              <p>© Copyright 2024 ZE-store. Design by ZE</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
