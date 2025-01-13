b1b9f0ec27bd38b12e86d7cdc596d9ee30125245
elkhatybzakariae <elkhatyb.zakariae@gmail.com>
2025-01-13 12:01:03 +0100

    a

--- app/Filament/Resources/CategorieResource.php
+++ app/Filament/Resources/CategorieResource.php
@@ -47,6 +47,7 @@ class CategorieResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
