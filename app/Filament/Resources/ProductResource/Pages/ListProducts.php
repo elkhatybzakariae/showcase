567007474ee6b4a1065f461f932ccb4893088bfd
elkhatybzakariae <elkhatyb.zakariae@gmail.com>
2025-01-18 11:21:43 +0100

    add

--- app/Filament/Resources/ProductResource/Pages/ListProducts.php
+++ app/Filament/Resources/ProductResource/Pages/ListProducts.php
@@ -5,6 +5,9 @@ namespace App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Resources\Pages\ListRecords\Tab;
+use Illuminate\Contracts\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 
 class ListProducts extends ListRecords
 {
@@ -14,6 +17,19 @@ class ListProducts extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return[
+            'all'=>Tab::make(),
+            'valider'=>Tab::make()->modifyQueryUsing(function(EloquentBuilder $query){
+                return $query->where('valider', true);
+            }),
+            'not valider'=>Tab::make()->modifyQueryUsing(function(EloquentBuilder $query){
+                return $query->where('valider', false);
+            }),
         ];
     }
 }
