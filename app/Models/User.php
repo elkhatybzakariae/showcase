fd029e9ee3be31579b2fc65710eedcb9db5f28a6
elkhatybzakariae <elkhatyb.zakariae@gmail.com>
2025-01-20 11:08:31 +0100

    add

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,24 +3,33 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->email='User102@gmail.com';
+        // return true;
+    }
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
 
-     protected $table = 'users';
+    protected $table = 'users';
     //  protected $primaryKey = 'id_Ad';
-     public $incrementing = true;
-     public $timestamps = true;
+    public $incrementing = true;
+    public $timestamps = true;
     protected $fillable = [
         'name',
         'email',
