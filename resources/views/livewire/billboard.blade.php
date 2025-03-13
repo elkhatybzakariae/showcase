5b9b845287e57c63f87675dec3bbf4f2b404edb6
elkhatybzakariae <elkhatyb.zakariae@gmail.com>
2025-03-13 16:25:04 +0000

    add

--- resources/views/livewire/billboard.blade.php
+++ resources/views/livewire/billboard.blade.php
@@ -1,81 +1,26 @@
-{{-- <section id="billboard" class="position-relative overflow-hidden bg-light-blue">
-    <div class="swiper main-swiper">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <div class="container">
-                    <div class="row d-flex align-items-center">
-                        <div class="col-md-6">
-                            <div class="banner-content">
-                                <h1 class="display-2 text-uppercase text-dark pb-5">Your Products Are Great.</h1>
-                                <a href="shop.html" class="btn btn-medium btn-dark text-uppercase btn-rounded-none">Shop
-                                    Product</a>
-                            </div>
-                        </div>
-                        <div class="col-md-5">
-                            <div class="image-holder">
-                                <img src="{{ asset('storage/images/banner-image.png') }}" alt="banner">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="swiper-slide">
-                <div class="container">
-                    <div class="row d-flex flex-wrap align-items-center">
-                        <div class="col-md-6">
-                            <div class="banner-content">
-                                <h1 class="display-2 text-uppercase text-dark pb-5">Technology Hack You Won't Get
-                                </h1>
-                                <a href="shop.html" class="btn btn-medium btn-dark text-uppercase btn-rounded-none">Shop
-                                    Product</a>
-                            </div>
-                        </div>
-                        <div class="col-md-5">
-                            <div class="image-holder">
-                                <img src="{{ asset('storage/images/banner-image.png') }}" alt="banner">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="swiper-icon swiper-arrow swiper-arrow-prev">
-        <svg class="chevron-left">
-            <use xlink:href="#chevron-left" />
-        </svg>
-    </div>
-    <div class="swiper-icon swiper-arrow swiper-arrow-next">
-        <svg class="chevron-right">
-            <use xlink:href="#chevron-right" />
-        </svg>
-    </div>
-</section> --}}
-
-
 <script src="https://cdn.jsdelivr.net/npm/tailwindcss-cdn@3.4.1/tailwindcss.js"></script>
 <script src="//unpkg.com/alpinejs" defer></script>
 <div x-data="swipeCards()" x-init="let isDown = false;
-let startX;
-let scrollLeft;
-$el.addEventListener('mousedown', (e) => {
-    isDown = true;
-    startX = e.pageX - $el.offsetLeft;
-    scrollLeft = $el.scrollLeft;
-});
-$el.addEventListener('mouseleave', () => {
-    isDown = false;
-});
-$el.addEventListener('mouseup', () => {
-    isDown = false;
-});
-$el.addEventListener('mousemove', (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - $el.offsetLeft;
-    const walk = (x - startX) * 1;
-    $el.scrollLeft = scrollLeft - walk;
-});" class="overflow-x-scroll scrollbar-hide mb-4  relative px-0.5"
+            let startX;
+            let scrollLeft;
+            $el.addEventListener('mousedown', (e) => {
+                isDown = true;
+                startX = e.pageX - $el.offsetLeft;
+                scrollLeft = $el.scrollLeft;
+            });
+            $el.addEventListener('mouseleave', () => {
+                isDown = false;
+            });
+            $el.addEventListener('mouseup', () => {
+                isDown = false;
+            });
+            $el.addEventListener('mousemove', (e) => {
+                if (!isDown) return;
+                e.preventDefault();
+                const x = e.pageX - $el.offsetLeft;
+                const walk = (x - startX) * 1;
+                $el.scrollLeft = scrollLeft - walk;
+                });" class="overflow-x-scroll scrollbar-hide mb-4  relative px-0.5"
     style="overflow-y: hidden;">
     <div class="flex snap-x snap-mandatory gap-4 mt-5" style="width: max-content;">
         <template x-for="card in cards" :key="card.id">
@@ -103,13 +48,14 @@ $el.addEventListener('mousemove', (e) => {
     </div>
 </div>
 <script>
-    var lastProducts = {!! $lastProductsJson !!};
-    var cards = @json($cards);
-    console.log(cards);
+    // var lastProducts = {!! $lastProductsJson !!};
+    // var cards = @json($cards);
+    // console.log(cards);
 
     function swipeCards() {
         return {
-            cards: cards,
+            // cards: cards,
+            cards: @json($cards),
             addToCart(product) {
                 // Implement your add to cart logic here
                 console.log('Adding to cart:', product);
